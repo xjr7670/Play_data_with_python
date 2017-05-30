e194bd54fd7c6558e3aef072b1a818a5f405efdf
Cavin <xjr30226@126.com>
2017-05-30 11:34:29 +0800

    第一周代码

---
+++ Chapter1/plot.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 22 19:40:46 2017
+
+@author: cavin
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+t = np.arange(0, 0, 0.1)
+plt.plot(t, t)
